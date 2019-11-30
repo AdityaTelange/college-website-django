cbb0227b8e20f934ce7ba3dc798c6090759a908c
Aditya Telange <21258296+AdityaTelange@users.noreply.github.com>
2019-12-01 00:58:25 +0530

    add actions; display list for admin

--- examcell/admin.py
+++ examcell/admin.py
@@ -8,13 +8,26 @@ from .models import (
 
 class ResultAdmin(admin.ModelAdmin):
     # ref:https://stackoverflow.com/a/36940373
-    search_fields = ("type__name", "year__name", "branch__name", "pattern_yr__name", "semester__name", "pattern__name",
-                     "exam_month__name", "exam_year")
+    search_fields = (
+        "type__name", "year__name", "branch__name", "pattern_year__name", "semester__name", "pattern__name",
+        "exam_month__name", "exam_year"
+    )
+    list_filter = ('type', 'year', 'branch')
+    list_display = (
+        ['type', 'year', 'branch', 'pattern_year', 'semester', 'pattern', 'exam_month', 'exam_year', 'is_active'])
 
     def get_readonly_fields(self, request, obj=None):
         if request.user.is_superuser:
             return self.readonly_fields
         return list(self.readonly_fields) + ["author", "pub_date"]
+
+    actions = ["mark_active", "mark_inactive"]
+
+    def mark_active(self, request, queryset):
+        queryset.update(is_active=True)
+
+    def mark_inactive(self, request, queryset):
+        queryset.update(is_active=False)
 
 
 class TypeAdmin(admin.ModelAdmin):
