f3645bbb4551750b81e8677d674959940a536be0
Aditya Telange <21258296+AdityaTelange@users.noreply.github.com>
2019-11-30 23:52:23 +0530

    miscellaneous

--- reset_all.sh
+++ reset_all.sh
@@ -1,5 +1,7 @@
 rm db.sqlite3
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc" -delete
+find . -path "media/*" -delete
+
 bash cfg.sh
 python3 manage.py createsuperuser --email admin@admin.com
---
+++ root/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "College Site Admin"
+admin.site.site_title = "College Site Admin"
+admin.site.index_title = "Welcome to College Site Admin Dashboard"
