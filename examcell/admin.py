1abe01a8f0f45ad9c67c5a19d967c1b56d95a685
Aditya Telange <21258296+AdityaTelange@users.noreply.github.com>
2019-11-30 23:42:40 +0530

    added object oriented fields for Result model, corresponding changes to admin; rm slug,absolute_url as unnecessary

--- examcell/admin.py
+++ examcell/admin.py
@@ -1,20 +1,30 @@
 from django.contrib import admin
 
-from .models import Result
-
-
-# admin.site.register(Result)
+from .models import (
+    Result, ResultTypeTag, ResultBranchTag, ResultPatternYrTag, ResultPatternTag, ResultSemesterTag, ResultExamMonthTag,
+    ResultYearTag
+)
 
 
 class ResultAdmin(admin.ModelAdmin):
-    prepopulated_fields = {
-        "slug": ("type", "year", "branch", "pattern_yr", "semester", "pattern", "exam_month", "exam_year")}
-    search_fields = ("type", "year", "branch", "pattern_yr", "semester", "pattern", "exam_month", "exam_year")
+    # ref:https://stackoverflow.com/a/36940373
+    search_fields = ("type__name", "year__name", "branch__name", "pattern_yr__name", "semester__name", "pattern__name",
+                     "exam_month__name", "exam_year")
 
     def get_readonly_fields(self, request, obj=None):
         if request.user.is_superuser:
             return self.readonly_fields
-        return list(self.readonly_fields) + ["slug", "branch"]
+        return list(self.readonly_fields) + ["author", "pub_date"]
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.readonly_fields
+        return list(self.readonly_fields) + ["name"]
 
     def get_prepopulated_fields(self, request, obj=None):
         if request.user.is_superuser:
@@ -24,3 +34,10 @@ class ResultAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Result, ResultAdmin)
+admin.site.register(ResultTypeTag, TypeAdmin)
+admin.site.register(ResultBranchTag, TypeAdmin)
+admin.site.register(ResultPatternYrTag, TypeAdmin)
+admin.site.register(ResultPatternTag, TypeAdmin)
+admin.site.register(ResultSemesterTag, TypeAdmin)
+admin.site.register(ResultExamMonthTag, TypeAdmin)
+admin.site.register(ResultYearTag, TypeAdmin)
--- examcell/models.py
+++ examcell/models.py
@@ -5,7 +5,6 @@ import uuid
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from django.urls import reverse
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
@@ -44,20 +43,87 @@ class ResultQuerySet(models.QuerySet):
         return self.get(slug=slug)
 
 
+class TagManager(models.Manager):
+    def get_by_natural_key(self, slug):
+        return self.get(slug=slug)
+
+
+class ResultTypeTag(models.Model):
+    name = models.CharField(max_length=64)
+    objects = TagManager()
+
+    def __str__(self):
+        return self.name
+
+
+class ResultBranchTag(models.Model):
+    name = models.CharField(max_length=64)
+    objects = TagManager()
+
+    def __str__(self):
+        return self.name
+
+
+class ResultPatternYrTag(models.Model):
+    name = models.CharField(max_length=64)
+    objects = TagManager()
+
+    def __str__(self):
+        return self.name
+
+
+class ResultPatternTag(models.Model):
+    name = models.CharField(max_length=64)
+    objects = TagManager()
+
+    def __str__(self):
+        return self.name
+
+
+class ResultSemesterTag(models.Model):
+    name = models.CharField(max_length=64)
+    objects = TagManager()
+
+    def __str__(self):
+        return self.name
+
+
+class ResultExamMonthTag(models.Model):
+    name = models.CharField(max_length=64)
+    objects = TagManager()
+
+    def __str__(self):
+        return self.name
+
+
+class ResultYearTag(models.Model):
+    name = models.CharField(max_length=64)
+    objects = TagManager()
+
+    def __str__(self):
+        return self.name
+
+
 class Result(models.Model):
     # sample : Revaluation Result BE (Mechanical Engg. R-2012)_Sem-VIII_CBGS_May-2019
-    type = models.CharField(choices=(('Regular', 'Regular'), ('Revaluation', 'Revaluation')), max_length=50)
-    branch = models.CharField(choices=(), max_length=64)
-    pattern_yr = models.CharField(choices=(), max_length=64)
-    pattern = models.CharField(choices=(), max_length=64, help_text=_("ex. CBCS, CBGS"))
-    semester = models.CharField(choices=(), max_length=64)
-    exam_month = models.CharField(choices=(('DEC', 'DEC'), ('MAY', 'MAY')), max_length=10, help_text=_("ex. DEC, MAY"))
+    type = models.ForeignKey(ResultTypeTag, blank=False, on_delete=models.CASCADE,
+                             help_text=_("ex. Regular, Revaluation"))
+    year = models.ForeignKey(ResultYearTag, blank=False, on_delete=models.CASCADE, help_text=_("ex. FE, SE"))
+    branch = models.ForeignKey(ResultBranchTag, blank=False, on_delete=models.CASCADE,
+                               help_text=_("ex. Computer, Electrical"))
+    pattern_year = models.ForeignKey(ResultPatternYrTag, blank=False, on_delete=models.CASCADE,
+                                     help_text=_("ex. R-2012, R-2016"))
+    semester = models.ForeignKey(ResultSemesterTag, blank=False, on_delete=models.CASCADE,
+                                 help_text=_("ex. SemII, SemIV"))
+    pattern = models.ForeignKey(ResultPatternTag, blank=False, on_delete=models.CASCADE,
+                                help_text=_("ex. CBCS, CBGS"))
+    exam_month = models.ForeignKey(ResultExamMonthTag, blank=False, on_delete=models.CASCADE,
+                                   help_text=_("ex. DEC, MAY"))
     # ref : https://stackoverflow.com/questions/49051017/year-field-in-django
-    exam_year = models.IntegerField(validators=[MinValueValidator(2000), max_value_current_year])
+    exam_year = models.IntegerField(default=current_year(),
+                                    validators=[MinValueValidator(2000), max_value_current_year])
+    author = models.ForeignKey(User, default=User, on_delete=models.CASCADE, )
 
-    year = models.CharField(choices=(('FE', 'FE'), ('SE', 'SE'), ('TE', 'TE'), ('BE', 'BE')), max_length=50)
-    slug = models.SlugField(max_length=48)
-    author = models.ForeignKey(User, default=User, on_delete=models.CASCADE)
     result_file = models.FileField(default='', validators=[ext_validators], upload_to=get_file_path)
     is_active = models.BooleanField(
         help_text=_(
@@ -65,7 +131,7 @@ class Result(models.Model):
             "Note that administrators (like yourself) are allowed to preview "
             "inactive entries whereas the general public aren't."
         ),
-        default=False,
+        default=True,
     )
     pub_date = models.DateTimeField(
         verbose_name=_("Publication date"),
@@ -84,17 +150,9 @@ class Result(models.Model):
 
     def __str__(self):
         return '-'.join(
-            [self.type, self.year, self.branch, self.pattern_yr, self.semester, self.pattern, self.exam_month,
+            [str(self.type), str(self.year), str(self.branch), str(self.pattern_yr), str(self.semester),
+             str(self.pattern), str(self.exam_month),
              str(self.exam_year)])
-
-    def get_absolute_url(self):
-        kwargs = {
-            'year': self.pub_date.year,
-            'month': self.pub_date.strftime('%b').lower(),
-            'day': self.pub_date.strftime('%d').lower(),
-            'slug': self.slug,
-        }
-        return reverse('result_entry', kwargs=kwargs)
 
     def is_published(self):
         """
